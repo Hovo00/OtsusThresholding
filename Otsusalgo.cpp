c737cf8714c4125318bc37c6412e435b7316c775
Hovo <hovikgrigroyan2000@gmail.com>
2022-07-15 21:34:40 -0700

    comm

--- Otsusalgo.cpp
+++ Otsusalgo.cpp
@@ -4,30 +4,31 @@
 #include <iostream>
 
 
-int main(){
+int main() {
 
-        cv::Mat image = cv::imread("apple.png", cv::IMREAD_GRAYSCALE);
+        cv::Mat image = cv::imread("Images.jpg", cv::IMREAD_GRAYSCALE);
 	cv::Mat dest;
 	cv::Mat hist;
 	int histSize = 255;
-	float range[] = { 0, 256}; //the upper boundary is exclusive
-        const float* histRange[] = { range };
+	float range[] = {0, 256}; //the upper boundary is exclusive
+        const float* histRange[] = {range};
 	bool uniform = true, accumulate = false;
-	cv::calcHist(&image, 1, 0, cv::Mat(), hist, 1, &histSize, histRange, uniform, accumulate );
-	std::cout<<image.cols<<" "<<image.rows<<std::endl;
+	cv::calcHist(&image, 1, 0, cv::Mat(), hist, 1, &histSize, histRange, uniform, accumulate);
+	std::cout << image.cols << " " << image.rows << std::endl;
 	/*
 	int sum = 0;
 	for(int i = 0; i < 255; ++i){
 	std::cout<<hist.at<float>(i)<<" ";
 	sum += hist.at<float>(i);
 	}
-	std::cout<<std::endl;
-	std::cout<<sum<<" "<<image.cols * image.rows;
+	std::cout << std::endl;
+	std::cout <<sum << " " << image.cols * image.rows;
 	*/
 	double omega1 = 0, omega2 = 0;
 	double sigmaB = 0;
         long double mean1, mean2;
 	double max_sigmaB = 0;
+	int thresh = 0;
 	for(int i = 0; i < 256; ++i){
 	    mean1 = mean2 = 0;
 	    omega1 = omega2 = 0;
@@ -40,7 +41,7 @@ int main(){
 	    mean1 /= count;
 	    //std::cout<<mean1<<" ";
 	    omega1 /= (image.cols * image.rows);
-	    std::cout<<omega1<<" ";
+	    //std::cout<<omega1<<" ";
 	    count = 0;
 	    for(int j = i + 1; j < 256; ++j){
 	        omega2 += hist.at<float>(j);
@@ -52,11 +53,22 @@ int main(){
 	    }
 	    //std::cout<<mean2<<" ";
 	    omega2 /= (image.cols * image.rows);
-	    std::cout<<omega2<<" ";
-	    sigmaB = omega1 * omega2 * ((mean1 - mean2)* (mean1 - mean2));
-	    if (sigmaB > max_sigmaB) max_sigmaB = sigmaB;
+	    //std::cout<<omega2<<" ";
+	    sigmaB = omega1 * omega2 * ((mean1 - mean2) * (mean1 - mean2));
+	    if (sigmaB > max_sigmaB) { 
+		max_sigmaB = sigmaB;
+		thresh = i;
+	    }
 	}
 	std::cout<<"\n\n\n";
-	std::cout<<max_sigmaB;
+	std::cout << thresh;
+	cv::Mat destik;
+	cv::Mat destik2;
+	cv::threshold(image, destik, thresh, 255, cv::THRESH_BINARY);
+	cv::threshold(image, destik2, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
+	cv::imshow("myalgo", destik);
+	cv::imshow("opencv-algo", destik2);
+	cv::imshow("image", image);
+	cv::waitKey(0);	
 
 }
